8f307dc09fb5868facfe5c8143e5a7ae6e4c3b6c
Dennis van der Vlugt <dtrace82@gmail.com>
2019-10-11 22:01:02 +0200

    mix with others

--- packages/video_player/ios/Classes/VideoPlayerPlugin.m
+++ packages/video_player/ios/Classes/VideoPlayerPlugin.m
@@ -448,7 +448,10 @@ static inline CGFloat radiansToDegrees(CGFloat radians) {
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"init" isEqualToString:call.method]) {
     // Allow audio playback when the Ring/Silent switch is set to silent
-    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback
+                                             withOptions:AVAudioSessionCategoryOptionMixWithOthers
+                                                   error:nil];
+    [[AVAudioSession sharedInstance] setActive:TRUE error:nil];
 
     for (NSNumber* textureId in _players) {
       [_registry unregisterTexture:[textureId unsignedIntegerValue]];
